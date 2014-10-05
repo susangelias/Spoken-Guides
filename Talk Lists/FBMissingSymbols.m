c907b92ce0a24a74144d5c8d736e995d99445f6e
Susan Elias <susangelias@gmail.com>
2014-10-05 19:05:40 -0400

    Updated Parse framework from version 1.2.1 to 1.4.1

---
+++ Talk Lists/FBMissingSymbols.m
@@ -0,0 +1,31 @@
+//
+//  FBMissingSymbols.m
+//  Talk Lists
+//
+//  Created by Susan Elias on 8/22/14.
+//  Copyright (c) 2014 GriffTech. All rights reserved.
+//
+
+NSString *FBTokenInformationExpirationDateKey = @"";
+NSString *FBTokenInformationTokenKey = @"";
+NSString *FBTokenInformationUserFBIDKey = @"";
+@interface FBAppCall:NSObject
+@end
+@implementation FBAppCall
+@end
+@interface FBRequest:NSObject
+@end
+@implementation FBRequest
+@end
+@interface FBSession:NSObject
+@end
+@implementation FBSession
+@end
+@interface FBSessionTokenCaching:NSObject
+@end
+@implementation FBSessionTokenCaching
+@end
+@interface FBSessionTokenCachingStrategy:NSObject
+@end
+@implementation FBSessionTokenCachingStrategy
+@end
