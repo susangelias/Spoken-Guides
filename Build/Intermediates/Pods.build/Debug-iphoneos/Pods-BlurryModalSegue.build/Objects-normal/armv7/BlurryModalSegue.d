36db23f5b376f10d464f1a372efd486921b68f2e
Susan Elias <susangelias@gmail.com>
2014-11-11 19:24:39 -0500

    Corrected Architecture settings for Talk Lists and Pods

--- Build/Intermediates/Pods.build/Debug-iphoneos/Pods-BlurryModalSegue.build/Objects-normal/armv7/BlurryModalSegue.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/susanelias/Documents/Developer/Talk\ Lists/Pods/BlurryModalSegue/BlurryModalSegue/BlurryModalSegue.m \
-  /Users/susanelias/Documents/Developer/Talk\ Lists/Pods/BlurryModalSegue/BlurryModalSegue/BlurryModalSegue.h \
-  /Users/susanelias/Documents/Developer/Talk\ Lists/Pods/BuildHeaders/UIImage+BlurredFrame/UIImage+ImageEffects.h \
-  /Users/susanelias/Documents/Developer/Talk\ Lists/Pods/BuildHeaders/MZAppearance/MZAppearance.h
